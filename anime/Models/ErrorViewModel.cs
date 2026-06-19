294b56764c78ce2d241c7e6bd4a22615232302df
Administrator <Administrator@PMT2024>
2026-06-19 15:17:57 +0700

    done

---
+++ anime/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace anime.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
